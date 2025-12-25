0d5592158c623e92e7d51d2284b51b56f92bb766
FyreByrns <horace5413@gmail.com>
2025-12-25 14:44:34 -0800

    use x/z instead of x/y

--- src/main/java/org/fyrebyrns/narya/mewg/MapPosition.java
+++ src/main/java/org/fyrebyrns/narya/mewg/MapPosition.java
@@ -1,6 +1,13 @@
 package org.fyrebyrns.narya.mewg;
 
-public record MapPosition(int x, int y) {
+public record MapPosition(int x, int z) {
+    public MapPosition modify(int modX, int modZ) {
+        return new MapPosition(this.x() + modX, this.z() + modZ);
+    }
 
+    public MapPosition north() { return modify(0, -1); }
+    public MapPosition south() { return modify(0, 1); }
+    public MapPosition east() { return modify(1, 0); }
+    public MapPosition west() { return modify(-1, 0); }
 }
 
