e7aed2d0aaa80dfdee7649d0113465ae87cff4c0
FyreByrns <horace5413@gmail.com>
2025-12-25 16:22:18 -0800

    more MathHelper functions

--- src/main/java/org/fyrebyrns/narya/mewg/MathHelper.java
+++ src/main/java/org/fyrebyrns/narya/mewg/MathHelper.java
@@ -4,4 +4,15 @@ public class MathHelper {
     public static double lerp(double start, double end, double amount) {
         return (start * (1.0 - amount)) + (end * amount);
     }
+
+    public static double sqrt(double num) {
+        return Math.sqrt(num);
+    }
+    public static double square(double num) {
+        return num * num;
+    }
+
+    public static double distance(double x1, double y1, double x2, double y2) {
+        return sqrt(square(x2 - x1) + square(y2 - y1));
+    }
 }
