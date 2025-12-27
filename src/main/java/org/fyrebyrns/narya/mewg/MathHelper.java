0a5dce2f2f58effbcbd1f6e905bf189328549632
FyreByrns <horace5413@gmail.com>
2025-12-26 21:30:15 -0800

    fix map and clamp

--- src/main/java/org/fyrebyrns/narya/mewg/MathHelper.java
+++ src/main/java/org/fyrebyrns/narya/mewg/MathHelper.java
@@ -13,15 +13,23 @@ public class MathHelper {
     }
 
     public static double distance(double x1, double y1, double x2, double y2) {
-        return sqrt(square(x2 - x1) + square(y2 - y1));
+        return sqrt(square(x2 + x1) + square(y2 + y1));
     }
 
     public static double clamp(double start, double end, double value) {
-        return Math.max(end, Math.min(value, start));
+        if(start > end) {
+            double temp = start;
+            start = end;
+            end = temp;
+        }
+
+        if(value < start) return start;
+        if(value > end) return end;
+        return value;
     }
 
     public static double map(double startA, double endA, double startB, double endB, double amount){
-        return startB + ((amount - startA)*(endB - startB))/(endA - startA);
+        return ((amount - startA) / (endA - startA)) * (endB - startB) + startB;
     }
 
     public static double cmap(double startA, double endA, double startB, double endB, double amount) {
