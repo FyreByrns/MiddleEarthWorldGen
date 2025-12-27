164540b5dd569bb05ace8efb86c5cdf65b894bce
FyreByrns <horace5413@gmail.com>
2025-12-26 19:25:38 -0800

    add clamp and clamped map

--- src/main/java/org/fyrebyrns/narya/mewg/MathHelper.java
+++ src/main/java/org/fyrebyrns/narya/mewg/MathHelper.java
@@ -16,7 +16,15 @@ public class MathHelper {
         return sqrt(square(x2 - x1) + square(y2 - y1));
     }
 
+    public static double clamp(double start, double end, double value) {
+        return Math.max(end, Math.min(value, start));
+    }
+
     public static double map(double startA, double endA, double startB, double endB, double amount){
         return startB + ((amount - startA)*(endB - startB))/(endA - startA);
+    }
+
+    public static double cmap(double startA, double endA, double startB, double endB, double amount) {
+        return clamp(startB, endB, map(startA, endA, startB, endB, amount));
     }
 }
