8e0e95783748a144cbec623090e03d8644b198d5
FyreByrns <horace5413@gmail.com>
2026-01-06 19:47:20 -0800

    add /metp command, to teleport to a specific x/z map coordinate

--- src/main/java/org/fyrebyrns/narya/mewg/Mewg.java
+++ src/main/java/org/fyrebyrns/narya/mewg/Mewg.java
@@ -1,12 +1,20 @@
 package org.fyrebyrns.narya.mewg;
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.permissions.Permissions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static net.minecraft.commands.Commands.argument;
+import static net.minecraft.commands.Commands.literal;
 
 public class Mewg implements ModInitializer {
     public static final String MOD_ID = "narya";
@@ -40,6 +48,22 @@ public class Mewg implements ModInitializer {
                 Identifier.fromNamespaceAndPath(MOD_ID, "mesurface"),
                 MESurface.CODEC
         );
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(literal("metp")
+                    .requires(source -> source.permissions().hasPermission(Permissions.COMMANDS_GAMEMASTER))
+                    .then(argument("location", BlockPosArgument.blockPos())
+                            .executes(ctx -> {
+                                BlockPos pos = BlockPosArgument.getBlockPos(ctx, "location");
+                                ctx.getSource().getPlayerOrException().teleportTo(
+                                        pos.getX() * LOTRMap.BLOCKS_PER_MAP_CELL,
+                                        pos.getY(),
+                                        pos.getZ() * LOTRMap.BLOCKS_PER_MAP_CELL);
+                                return 1;
+                            })
+                    )
+            );
+        });
 
         DefaultLOTRBiomes.init();
     }
